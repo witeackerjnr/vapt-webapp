5e2c74cb8b46394b0b0f506300f12cf6bf512ea9
witeackerjnr <124600702+witeackerjnr@users.noreply.github.com>
2025-03-04 14:27:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Local API Base URL
-const apiBaseUrl = "https://ec51-102-89-75-204.ngrok-free.app";  // Only local Flask API
+const apiBaseUrl = "https://9a38-102-89-75-204.ngrok-free.app";  // Only local Flask API
 
 console.log("Using API Base URL:", apiBaseUrl);  // Debugging
 
